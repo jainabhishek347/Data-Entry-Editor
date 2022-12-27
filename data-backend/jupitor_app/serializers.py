b408e353cc79caa3461a8dcf17aefd0c5b1e4161
abhishek jain <jainabhishek347@gmail.com>
2022-12-27 19:28:06 +0530

    update filed

--- data-backend/jupitor_app/serializers.py
+++ data-backend/jupitor_app/serializers.py
@@ -41,11 +41,11 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'name',  'email', 'password')
+        fields = ('id', 'name',  'email', 'password','username')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'], password=validated_data['password'],
+        user = User.objects.create_user(validated_data['username'], password=validated_data['password'],
                                         email=validated_data.get('email'), first_name=validated_data['first_name'])
 
         return user
